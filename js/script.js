5df5477d5a8325c4815de4c3552c9b22b8851ba5
Md Saiful Islam Sazin <mdsaiful510@gmail.com>
2020-09-26 19:30:18 +0600

    added the count tax and total

--- js/script.js
+++ js/script.js
@@ -1,3 +1,79 @@
+//
+//
+//
+// Function for the shopping card more dynamic and profesonal developer
+
+function handleProductCount(product, isIncrease) {
+    var productCount = document.getElementById(product + '-count');
+    const caseCount = parseInt(productCount.value);
+    let productNewCount = caseCount;
+
+    if (isIncrease == true) {
+        productNewCount = caseCount + 1;
+    }
+    if (isIncrease == false && caseCount > 0) {
+        productNewCount = caseCount - 1;
+    }
+    productCount.value = productNewCount;
+
+    if (product == 'case') {
+        productTotal = productNewCount * 60;
+    }
+    if (product == 'phone') {
+        productTotal = productNewCount * 1200;
+    }
+
+    document.getElementById(product + '-Total').innerText = '$' + productTotal;
+
+    calculateTotal();
+}
+
+
+function calculateTotal() {
+
+    const phoneInput = document.getElementById('phone-count');
+    const phoneCount = parseInt(phoneInput.value);
+
+    const caseInput = document.getElementById('case-count');
+    const caseCount = parseInt(caseInput.value);
+
+    // Total Price 
+    const totalPrice = phoneCount * 1200 + caseCount + 60;
+
+    document.getElementById('total-price').innerText = '$' + totalPrice;
+
+
+    // count tax
+    const tax = Math.round(totalPrice * 0.1);
+    document.getElementById('total-tax').innerText = '$' + tax;
+
+    // grand Total 
+
+    const grandTotal = totalPrice + tax;
+    document.getElementById('grand-total').innerText = '$' + grandTotal;
+}
+
+
+
+
+// function handlePhoneProductCount(isIncrease) {
+//     var caseInput = document.getElementById('phone-count');
+//     const caseCount = parseInt(caseInput.value);
+//     let caseNewCount = caseCount;
+
+//     if (isIncrease == true) {
+//         caseNewCount = caseCount + 1;
+//     }
+//     if (isIncrease == false && caseCount > 0) {
+//         caseNewCount = caseCount - 1;
+//     }
+//     caseInput.value = caseNewCount;
+//     const caseTotalCount = caseNewCount * 1200;
+//     document.getElementById('phone-Total').innerText = '$' + caseTotalCount;
+// }
+
+
+
 // Do it normal codding 
 
 // // plush button handler  
@@ -43,21 +119,4 @@
 // });
 
 
-// Function and conditon for increase and descrease 
-
-
-function handleProductCount(isIncrease) {
-    var caseInput = document.getElementById('case-count');
-    const caseCount = parseInt(caseInput.value);
-    let caseNewCount = caseCount;
-
-    if (isIncrease == true) {
-        caseNewCount = caseCount + 1;
-    }
-    if (isIncrease == false && caseCount > 0) {
-        caseNewCount = caseCount - 1;
-    }
-    caseInput.value = caseNewCount;
-    const caseTotalCount = caseNewCount * 59;
-    document.getElementById('caseTotal').innerText = '$' + caseTotalCount;
-}
+// Function and conditon for increase and descrease
